0fcdcabf2d419ebfb6bc97f47ec16931fddf646d
aceraceae <yackoo@ymail.com>
2016-08-12 00:21:06 +0200

    db working

--- db-handler.js
+++ db-handler.js
@@ -2,27 +2,13 @@
 const randomstring = require('randomstring');
 
 module.exports = {
-    records: [],
     postUrl(url, host, secure=false) {
-        const newUrl = link(url, host, secure);
-        this.records.push(newUrl);
-        return {"longUrl": newUrl.longUrl, "shortUrl": newUrl.shortUrl};
-    },
-    getUrl(url) {
-       for(let rec of this.records) {
-           console.log(rec);
-           if(rec.random === url) {
-               console.log(rec);
-               return rec.longUrl;
-           }
-       }
+
+        const longUrl = url;
+        const random = randomstring.generate(7);
+        const protocol = secure ? "https" : "http";
+        const shortUrl = `${protocol}://${host}/${random}`;
+
+        return { longUrl, shortUrl, random, host, protocol};
     }
 };
-
-function link(url, host, secure) {
-    const longUrl = url;
-    const random = randomstring.generate(7);
-    const protocol = secure ? "https" : "http";
-    const shortUrl = `${protocol}://${host}/${random}`;
-    return { longUrl, shortUrl, random, host, protocol};
-}
--- server.js
+++ server.js
@@ -17,35 +17,11 @@ app.use(express.static(path.resolve(__dirname)));
 app.use(matchUrl);
 
 
-/*
-
-// Create a collection we want to drop later
-
-  // Insert a bunch of documents for the index
-  collection.insertMany([{a:1}, {a:2}, {a:3}, {a:4}], {w:1}, function(err, result) {
-    test.equal(null, err);
-
-    // Create an index on the a field
-    collection.createIndex('a', {w:1}, function(err, indexName) {
-      test.equal("a_1", indexName);
-
-      // Peform a query, with explain to show we hit the query
-      collection.find({a:2}).explain(function(err, explanation) {
-        test.equal(null, err);
-        test.ok(explanation != null);
-
-        db.close();
-      });
-    });
-  });
-});
-*/
-
 MongoClient.connect('mongodb://localhost:27017/urls', (err, db) => {
     if(err) {
         console.error(err);
     }
-    const collection = db.collection('urls');
+    const col = db.collection('urls');
 
     app.get('/', (req,res) => {
         res.writeHead(200, {"Content-Type": "text/json"});
@@ -55,25 +31,33 @@ MongoClient.connect('mongodb://localhost:27017/urls', (err, db) => {
     app.get('/:short', (req, res) => {
         //get url from db;
         const queryUrl = req.params.short;
-        if(true) {
-            res.redirect(handleDb.getUrl(queryUrl));
-        } else {
-            res.end(`Link doesn't exist`);
-        }
+        const opts = { w:1, background: true, sparse: true, unique: true };
+        col.createIndex('random', opts, (err, idx) => {
+
+             if(err) { console.log(err); }
+
+             col.find({"random":queryUrl}).toArray((err, docs) => {
+                   if(err) {
+                     console.log(err);
+                     res.end(`Link doesn't exist`);
+                 }
+                 console.log(docs);
+                 res.redirect(docs[0].longUrl);
+              });
+        });
+
     });
 
     app.get('/new/:long', (req, res) => {
         const queryUrl = req.params.long.slice(1);
         //post new url to db;
         if(validUrl.isUri(queryUrl)) {
-           /*
-            collection.insertOne(handleDb.postUrl(queryUrl, req.headers.host, req.secure), (err, result) => {
+            col.insertOne(handleDb.postUrl(queryUrl, req.headers.host, req.secure), (err, result) => {
                 if(err) { console.error(err); }
-                console.log(result);
-
-                res.json(`Hello`);
+                const link = result.ops[0];
+                res.json({"longUrl": link.longUrl, "shortUrl": link.shortUrl});
             });
-            */
+
         } else {
             res.end(`Invalid URL`);
         }
